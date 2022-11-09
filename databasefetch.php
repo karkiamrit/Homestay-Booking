1417d8540edc8995625bb5b12f16f72d9740f67c
karkiamrit <amrit.karki2073@gmail.com>
2022-11-09 10:12:34 +0545

    js workup

--- databasefetch.php
+++ databasefetch.php
@@ -15,10 +15,14 @@
 ?>  
 <head>
     <style>
-        #formid{
-            background-color:red;
+        #formdeleteid{
+            background-color:orange;
+        }
+        .btn{
+            background-color:orange;
         }
     </style>
+    
 </head>
 <?php        
         if( $query_run)
@@ -27,14 +31,61 @@
             {
                 while($row=mysqli_fetch_assoc($query_run))
                 {
-                    echo "</br>"."regid=$row[Registerationno]";
-                    echo "</br>"."firstname=$row[fname]";
-                    echo "</br>"."lastname=$row[lname]";
-                    echo "</br>"."Location=$row[Location]";
+                    echo "</br>"."Username=$row[username]"."</br>";
+                    echo "</br>";
+                    echo "</br>"."First Name=$row[fname]"."</br>";
+                    echo "</br>";
+                    echo "</br>"."Last Name=$row[lname]"."</br>";
+                    echo "</br>";
+
                     $_SESSION['Regno']=$row['Registerationno'];
-                    echo '<form method="post" action="delete.php" >'.'<button type="submit" id="formid" style=" width: 100px; height:30px" value="Delete" >'.'Delete'."</button>".'</form>';
+                    
+                    echo "</br>"."Booking/Registration No=$row[Registerationno]";
+                    echo '<form method="post" id="formeditid" >'.'<button type="button" class="btn" id="btn" style=" width: 100px; height:30px" value="Edit"  onclick="showbuton(`form0`)">'.'Edit'."</button>".'</form>';
+                    echo '<form action="update.php" method="post" id="form0" style="display:none;"><input type="text" ><button type="submit" style="display:none;">Update</button> </form>';
+                    echo "</br>";
+
+                    echo "</br>"."No of Guests=$row[NoofGuests]";
+                    echo '<form method="post" id="formeditid" >'.'<button type="button" class="btn" id="btn" style=" width: 100px; height:30px" value="Edit"  onclick="showbuton(`form1`)">'.'Edit'."</button>".'</form>';
+                    echo '<form action="update.php" method="post" id="form1" style="display:none;"><input type="text" style="display:none;"><button type="submit" style="display:none;">Update</button></form> ';
+                    echo "</br>";
+                    
+                    echo "</br>"."Location=$row[Location]";
+                    echo '<form method="post" id="formeditid" >'.'<button type="button" class="btn" id="btn" style=" width: 100px; height:30px" value="Edit"  onclick="showbuton(`form2`)">'.'Edit'."</button>".'</form>';
+                    echo '<form action="update.php" method="post" id="form2" style="display:none;"><input type="text" style="display:none;"><button type="submit" style="display:none;">Update</button> </form>';
+                    echo "</br>";
+                    
+                    echo "</br>"."Departure Date=$row[DepartureDate]";
+                    echo '<form method="post" id="formeditid" >'.'<button type="button" class="btn" id="btn" style=" width: 100px; height:30px" value="Edit"  onclick="showbuton(`form3`)">'.'Edit'."</button>".'</form>';
+                    echo '<form action="update.php" method="post" id="form3" style="display:none;"><input type="text" style="display:none;"><button type="submit" style="display:none;">Update</button> </form>';
+                    echo "</br>";
+
+                    echo "</br>"."Date of Return=$row[DateofReturn]";
+                    echo '<form method="post" id="formeditid" >'.'<button type="button" class="btn" id="btn" style=" width: 100px; height:30px" value="Edit"  onclick="showbuton(`form4`)">'.'Edit'."</button>".'</form>';
+                    echo '<form action="update.php" method="post" id="form4" style="display:none;"><input type="text" style="display:none;"><button type="submit" style="display:none;">Update</button> </form>';
+                    echo "</br>";
+
+                    echo "</br>"."Travel Duration=$row[TravelDuration]";
+                    echo '<form method="post" id="formeditid" >'.'<button type="button" class="btn" id="btn" style=" width: 100px; height:30px" value="Edit"  onclick="showbuton(`form5`)">'.'Edit'."</button>".'</form>';
+                    echo '<form action="update.php" method="post" id="form5" style="display:none;"><input type="text" style="display:none;"><button type="submit" style="display:none;">Update</button> </form>';
+                    echo "</br>";
+                    
+                   
+                    echo '<form method="post" action="delete.php" >'.'<button type="submit" id="formdeleteid" style=" width: 200px; height:60px" value="Delete" >'.'Delete All Booking Details'."</button>".'</form>';
                 }
             }
             mysqli_free_result($query_run);
         }
+        
 ?>
+<script>
+    showbuton=(e)=>
+    {
+        var form=document.getElementById(e);
+        form.style.display='block';
+        console.log(e);
+        
+
+    }
+
+</script>
